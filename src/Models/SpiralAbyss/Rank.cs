9173580f40e9806adc0ae0ec4ab1cd693df51226
李卓强 <1137361788@qq.com>
2022-01-25 01:27:27 +0800

    [feat] ctrl c-v spiral abyss models.

---
+++ src/Models/SpiralAbyss/Rank.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace DGP.Genshin.MiHoYoAPI.Record.SpiralAbyss
+{
+    /// <summary>
+    /// 角色数值排行信息
+    /// </summary>
+    public class Rank
+    {
+        [JsonPropertyName("avatar_id")] public int AvatarId { get; set; }
+        [JsonPropertyName("avatar_icon")] public string? AvatarIcon { get; set; }
+        [JsonPropertyName("value")] public int Value { get; set; }
+        [JsonPropertyName("rarity")] public int Rarity { get; set; }
+    }
+}
