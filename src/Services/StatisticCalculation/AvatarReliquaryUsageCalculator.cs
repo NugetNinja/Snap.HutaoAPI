39ae7ca98de95183a9d43b58c1d000d33d751e10
李卓强 <1137361788@qq.com>
2022-01-27 15:23:21 +0800

    [fix] missing rate statistics.

--- src/Services/StatisticCalculation/AvatarReliquaryUsageCalculator.cs
+++ src/Services/StatisticCalculation/AvatarReliquaryUsageCalculator.cs
@@ -16,35 +16,38 @@ namespace Snap.Genshin.Website.Services.StatisticCalculation
 
         public async Task Calculate()
         {
-            var ansDic = new Dictionary<int, Dictionary<string, int>>();
+            var ansDic = new Dictionary<int, Dictionary<string, int>>(128);
+            var countDic = new Dictionary<int, int>(128);
             foreach (var avatar in dbContext.AvatarDetails)
             {
                 var sets = avatar.ReliquarySets.Where(set => set.Count >= 4);
                 if (!sets.Any()) sets = avatar.ReliquarySets.Where(set => set.Count >= 2);
                     
                 if (!ansDic.ContainsKey(avatar.AvatarId))
-                    ansDic.Add(avatar.AvatarId, new Dictionary<string, int>());
+                    ansDic.Add(avatar.AvatarId, new Dictionary<string, int>(32));
                 foreach(var set in sets)
                 {
                     if (!ansDic[avatar.AvatarId].ContainsKey(set.UnionId))
                         ansDic[avatar.AvatarId].Add(set.UnionId, 0);
                     ansDic[avatar.AvatarId][set.UnionId]++;
+                    countDic[avatar.AvatarId]++;
                 }
             }
 
             var result = from kv in ansDic
-                select new AvatarReliquaryUsage
-                {
-                    Avatar = kv.Key,
-                    ReliquaryUsage = kv.Value.Select(kv => new Rate<Models.SnapGenshin.AvatarReliquarySet>
-                    {
-                        Id = new Models.SnapGenshin.AvatarReliquarySet
-                        {
-                            Id = Convert.ToInt32(kv.Key.Split('-')[0]),
-                            Count = Convert.ToInt32(kv.Key.Split('-')[1])
-                        }
-                    })
-                };
+                         select new AvatarReliquaryUsage
+                         {
+                             Avatar = kv.Key,
+                             ReliquaryUsage = kv.Value.Select(kvp => new Rate<Models.SnapGenshin.AvatarReliquarySet>
+                             {
+                                 Id = new Models.SnapGenshin.AvatarReliquarySet
+                                 {
+                                     Id = Convert.ToInt32(kvp.Key.Split('-')[0]),
+                                     Count = Convert.ToInt32(kvp.Key.Split('-')[1])
+                                 },
+                                 Value = (double)kvp.Value / countDic[kv.Key]
+                             })
+                         };
 
             await statisticsProvider.SaveStatistics<AvatarReliquaryUsageCalculator>(result);
         }
