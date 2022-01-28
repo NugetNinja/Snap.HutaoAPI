4c6006eaed1008b6647f21ae65290237862e2bf6
李卓强 <1137361788@qq.com>
2022-01-28 15:45:00 +0800

    [fix] return null while no response data.

--- src/Models/ApiResponse/ControllerExtension.cs
+++ src/Models/ApiResponse/ControllerExtension.cs
@@ -8,12 +8,12 @@ namespace Snap.Genshin.Website.Models
             => new JsonResult(new ApiResponse<T>(ApiCode.Success, msg, data));
 
         public static IActionResult Success(this ControllerBase _, string msg)
-            => new JsonResult(new ApiResponse<object>(ApiCode.Success, msg, Array.Empty<object>()));
+            => new JsonResult(new ApiResponse<object?>(ApiCode.Success, msg, null));
 
         public static IActionResult Fail(this ControllerBase _, string msg)
-            => new JsonResult(new ApiResponse<object>(ApiCode.Fail, msg, Array.Empty<object>()));
+            => new JsonResult(new ApiResponse<object?>(ApiCode.Fail, msg, null));
 
         public static IActionResult Fail(this ControllerBase _, ApiCode code, string msg)
-            => new JsonResult(new ApiResponse<object>(code, msg, Array.Empty<object>()));
+            => new JsonResult(new ApiResponse<object?>(code, msg, null));
     }
 }
