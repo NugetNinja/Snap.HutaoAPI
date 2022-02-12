ed3100faadb38c2ad77ccb6305b49dca2220ee0a
李卓强 <1137361788@qq.com>
2022-02-12 13:53:52 +0800

    [fix] no 7

--- src/Services/StatisticCalculation/ActivedConstellationNumCalculator.cs
+++ src/Services/StatisticCalculation/ActivedConstellationNumCalculator.cs
@@ -27,7 +27,7 @@ namespace Snap.Genshin.Website.Services.StatisticCalculation
 
                 var countDic = new Dictionary<int, int>
                 {
-                    {0, 0},{1, 0},{2, 0},{3, 0},{4, 0},{5, 0},{6, 0},{7, 0}
+                    {0, 0},{1, 0},{2, 0},{3, 0},{4, 0},{5, 0},{6, 0}
                 };
 
                 foreach(var avatar in group)
