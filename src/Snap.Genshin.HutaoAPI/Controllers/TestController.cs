0ea6f5813ad746e90779203a6081ab7b76a10207
DismissedLight <1686188646@qq.com>
2022-05-31 14:39:57 +0800

    Create TestController.cs

---
+++ src/Snap.Genshin.HutaoAPI/Controllers/TestController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Snap.HutaoAPI.Services;
+
+namespace Snap.HutaoAPI.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class TestController : ControllerBase
+{
+    private readonly ILogger<TestController> logger;
+    private readonly GenshinStatisticsService statisticsService;
+
+    /// <summary>
+    /// 测试控制器
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="statisticsService"></param>
+    public TestController(ILogger<TestController> logger, GenshinStatisticsService statisticsService)
+    {
+        this.logger = logger;
+        this.statisticsService = statisticsService;
+    }
+
+    [HttpGet("[Action]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public async Task<IActionResult> Run()
+    {
+        logger.LogInformation("已触发测试数据更新...");
+
+        // 计算数据
+        await statisticsService.CalculateAllStatistics().ConfigureAwait(false);
+
+        return Ok();
+    }
+}
